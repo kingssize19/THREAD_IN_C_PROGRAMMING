50f024b3262303c540bd4d93c4d66c35a17684d9
Hayati Kılıç <83482374+kingssize19@users.noreply.github.com>
2024-10-01 14:09:11 +0300

    .

---
+++ Thread1/main1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void func1()
+{
+    while(1)
+    {
+        printf("func1 \n");
+        sleep(1);
+    }
+}
+
+void func2()
+{
+    while(1)
+    {
+        printf("func2 \n");
+        sleep(1);
+    }
+
+}
+
+int main()
+{
+
+    func1();        //func1 fonksiyonu içindeki while(1) içerisinden çıkamaz sadece ekrana func1 yazar.
+    func2();
+
+    return 0;
+}
